2abc89f995c2449c0c6b0e1db97b8366f9e2e893
RaudoR <rivaspaulino@outlook.com>
2019-12-10 22:08:10 +0000

    fixed negative numbers error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,8 +6,8 @@ if number >= 0:
 elif number < 0:
     mod = number % -10
 if mod > 5:
-    print('Last digit of ' + str(number) + ' is ' + str(mod) + ' and is greater than 5')
+    print('Last digit of {} is {} and is greater than 5'.format(number, mod))
 elif mod == 0:
-    print('Last digit of ' + str(number) + ' is ' + str(mod) + ' and is 0')
+    print('Last digit of {} is {} and is 0'.format(number, mod))
 elif mod < 6 and not 0:
-    print('Last digit of ' + str(number) + ' is ' + str(mod) + ' and is less than 6 and not 0')
+    print('Last digit of {} is {} and is less than 6 and not 0'.format(number, mod))
