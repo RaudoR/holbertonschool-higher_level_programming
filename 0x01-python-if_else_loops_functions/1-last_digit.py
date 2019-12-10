0c6e3867c413df1a60c3cec5533428a7863436bf
RaudoR <rivaspaulino@outlook.com>
2019-12-10 21:57:12 +0000

    fised negative numbers error

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number >= 0:
+    mod = number % 10
+elif number < 0:
+    mod = number % -10
+if mod > 5:
+    print('Last digit of ' + str(number) + ' is ' + str(mod) + ' and is greater than 5')
+elif mod == 0:
+    print('Last digit of ' + str(number) + ' is ' + str(mod) + ' and is 0')
+elif mod < 6 and not 0:
+    print('Last digit of ' + str(number) + ' is ' + str(mod) + ' and is less than 6 and not 0')
