0a6782f45e028968b54dd8a8811cefabed48c273
RaudoR <rivaspaulino@outlook.com>
2020-01-09 22:01:54 +0000

    fixed blank lines

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,9 +6,11 @@ class Square:
         elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -16,5 +18,6 @@ class Square:
         elif value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
+
     def area(self):
         return self.__size ** 2
