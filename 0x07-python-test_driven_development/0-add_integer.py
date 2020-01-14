c7bae65d423ce4ccfa5e27378698a3f4bf48fc47
RaudoR <rivaspaulino@outlook.com>
2020-01-14 04:44:42 +0000

    fixed blank lines

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+
+
 '''
    0-add_integer function
    Contains a function that adds two integers
 '''
 
+
 def add_integer(a, b=98):
+
     '''
     Python function to sum two integers and check if a/b is int & float
     '''
+
     if type(a) is int or type(a) is float:
         if type(b) is int or type(b) is float:
             return int(a) + int(b)
