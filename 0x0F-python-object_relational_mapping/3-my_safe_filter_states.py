ff2f2f7c7a8f0c17942edade0793d75f3a249ebe
RaudoR <rivaspaulino@outlook.com>
2020-03-29 19:54:20 +0000

    this script will display the state matching the argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name LIKE "%s" ORDER BY id'
-                .format(argv[4]))
+    cur.execute('SELECT * FROM states WHERE name = %s ORDER BY states.id',
+                (argv[4], ))
     for state in cur.fetchall():
             print(state)
 
