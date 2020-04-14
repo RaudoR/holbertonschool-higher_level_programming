dfa41000c414258898ed59182aaffcb9c76213eb
RaudoR <rivaspaulino@outlook.com>
2020-04-14 00:20:14 +0000

    new variable

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ body of the response.
 import requests
 from sys import argv
 if __name__ == "__main__":
-    r = requests.post(argv[1], {"email": argv[2]})
-    print(r.text)
+    response = requests.post(argv[1], {"email": argv[2]})
+    print(response.text)
