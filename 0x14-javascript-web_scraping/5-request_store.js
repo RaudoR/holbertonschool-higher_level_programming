20f2fc6fc80e8504b4eab43e24679b4c425dd519
RaudoR <rivaspaulino@outlook.com>
2020-05-13 01:16:33 +0000

    Write a script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let request = require('request');
-let fs = require('fs');
+const request = require('request');
+const fs = require('fs');
 request.get(process.argv[2]).on('error', function (err) {
   if (err) {
     console.log(err);
